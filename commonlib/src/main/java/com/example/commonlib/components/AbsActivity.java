732332510793bc93f0eb0f4a1f1124d4d3d30165
liudeyu <liudeyu@bytedance.com>
2018-01-26 19:52:38 +0800

    微博模块基本算完成

--- commonlib/src/main/java/com/example/commonlib/components/AbsActivity.java
+++ commonlib/src/main/java/com/example/commonlib/components/AbsActivity.java
@@ -179,8 +179,7 @@ public abstract class AbsActivity extends AppCompatActivity implements IComponen
     private void setFullScreenAndTransparent() {
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(option);
             getWindow().setNavigationBarColor(Color.TRANSPARENT);
